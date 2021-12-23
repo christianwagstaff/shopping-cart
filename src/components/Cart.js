2f91b7927ced1b5f613956e6749e4602b1ea395c
Christian <chris23681@gmail.com>
2021-12-22 22:08:45 -0800

    Cart works

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,24 @@
 import { ReactComponent as Close } from "../images/close.svg";
 import plantList from "./plantList";
-
-const cart = localStorage.getItem("cart");
+import "../styles/cart.css";
+import { useState } from "react";
 
 const Cart = () => {
-  console.log(localStorage.getItem("cart"));
+  const [cart, setCart] = useLocalStorage("cart", "");
   return (
     <div className="cart shop">
       <h1>Cart</h1>
-      <CartItem />
+      {[...cart].map((item) => {
+        console.log(item);
+        return (
+          <CartItem
+            setCart={setCart}
+            key={item.id}
+            id={item.id}
+            amount={item.quantity}
+          />
+        );
+      })}
       <CartTotal />
     </div>
   );
@@ -17,25 +27,88 @@ const Cart = () => {
 export default Cart;
 
 const CartItem = (props) => {
+  const setCart = props.setCart;
+  const selected = plantList.filter(
+    (plant) => plant.id === parseInt(props.id)
+  )[0];
+  function increaseAmount(id) {
+    let newAmount = props.amount + 1;
+    updateCart(id, newAmount);
+  }
+  function decreaseAmount(id) {
+    let newAmount = props.amount - 1;
+    if (newAmount === 0) {
+      console.log("is 0");
+      deleteItem(id);
+      return;
+    }
+    updateCart(id, newAmount);
+  }
+  function updateCart(id, amount) {
+    let storage = localStorage.getItem("cart");
+    let cart = JSON.parse(storage);
+    let newItem = { id: id, quantity: amount };
+    if (cart) {
+      if (cart.filter((e) => e.id === id).length > 0) {
+        let index = cart.findIndex((e) => e.id === id);
+        cart = replaceAt(cart, index, newItem);
+      } else {
+        cart.push(newItem);
+      }
+      setCart(cart);
+    } else {
+      let newCart = [newItem];
+      setCart(newCart);
+    }
+  }
+  const deleteItem = (id) => {
+    let storage = localStorage.getItem("cart");
+    let cart = JSON.parse(storage);
+    const oldArray = cart;
+    const selectedIndex = oldArray.findIndex((job) => job.id === id);
+    const newArray = [
+      ...oldArray.slice(0, selectedIndex),
+      ...oldArray.slice(selectedIndex + 1),
+    ];
+    setCart(newArray);
+  };
   return (
     <div className="cart-item">
-      <div className="image-container">
-        <img src={props.img} alt={props.name} />
+      <div className="image-container cart-image-container">
+        <img src={selected.img} alt={selected.name} />
       </div>
       <div className="cart-details">
-        <h3>{props.name}</h3>
-        <div className="price">{props.total}</div>
+        <div className="item-info">
+          <h3>{selected.name}</h3>
+          <div className="price-amount">{selected.price * props.amount}</div>
+        </div>
+        <div className="horizontal">
+          <div className="quantity">
+            <button
+              className="decreaseAmount"
+              onClick={() => {
+                decreaseAmount(props.id);
+              }}
+            >
+              -
+            </button>
+            <div>{props.amount}</div>
+            <button
+              className="increaseAmount"
+              onClick={() => {
+                increaseAmount(props.id);
+              }}
+            >
+              +
+            </button>
+          </div>
+          <Close
+            onClick={() => {
+              deleteItem(props.id);
+            }}
+          />
+        </div>
       </div>
-      <div className="quantity">
-        <button className="decreaseAmount" onClick={() => {}}>
-          -
-        </button>
-        <div>{props.amount}</div>
-        <button className="increaseAmount" onClick={() => {}}>
-          +
-        </button>
-      </div>
-      <Close />
     </div>
   );
 };
@@ -43,3 +116,42 @@ const CartItem = (props) => {
 const CartTotal = () => {
   return <div>Cart Total</div>;
 };
+function replaceAt(array, index, value) {
+  const ret = array.slice(0);
+  ret[index] = value;
+  return ret;
+}
+
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
